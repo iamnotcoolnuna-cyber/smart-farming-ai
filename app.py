77214bcd0b6aacdfbd59c8e4bf1f08f759002459
iamnotcoolnuna-cyber <iamnotcoolnuna@gmail.com>
2026-06-17 13:41:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,5 +49,7 @@ def home():
         chatbot_response=chatbot_response
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
